e5896fd65b3843a927eda05f5ca990a43eb9276e
Cassidy Matousek <Cassidy.Matousek@gmail.com>
2015-11-12 16:11:13 -0600

    test

---
+++ determineQuote.c
@@ -0,0 +1,19 @@
+/****************************************************
+
+
+
+
+
+
+*****************************************************/
+ #include <stdio.h>
+ #include <string.h>
+ #include <stdarg.h>
+ #include <stdlib.h>
+ #include "cs2123p5.h"
+
+ QuoteResult determineQuote(Tree tree, QuoteSelection quoteSelection){
+
+
+
+ }
